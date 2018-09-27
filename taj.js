1354d53d764cbcea1bd712d1a8bd9774a2974be3
mightynut <43552735+mightynut@users.noreply.github.com>
2018-09-28 00:16:38 +0300

    Tajam files
    Add files

---
+++ taj.js
@@ -0,0 +1,21 @@
+/*$(document).ready(function(){
+     $(function () {
+        var e = $(".mjs");
+        menu = $(".uljs"), menuHeight = menu.height(), $(e).on("click", function (e) {
+            e.preventDefault(), menu.slideToggle()
+        })
+    }), $(window).resize(function () {
+        $(window).width() > 320 && menu.is(":hidden") && menu.removeAttr("style")
+    }),
+
+
+     
+
+      $(function () {
+        $(window).scroll(function () {
+            0 != $(this).scrollTop() ? $("#toTop").fadeIn() : $("#toTop").fadeOut()
+        }), $("#toTop").click(function () {
+            $("body,html").animate({scrollTop: 0}, 800)
+        })
+    });
+})*/
